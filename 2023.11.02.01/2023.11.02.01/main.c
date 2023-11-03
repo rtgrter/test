191208bac6b728d659d6388d0c3fc82eb9baef91
rtgrter <2500651598@qq.com>
2023-11-03 23:09:06 +0800

    学习代码

--- 2023.11.02.01/2023.11.02.01/main.c
+++ 2023.11.02.01/2023.11.02.01/main.c
@@ -31,35 +31,89 @@
 //	//}
 //	//return 0;
 //}
-void print1(int arr[3][5], int r, int c)
-{
-	int i = 0;
-	for (i = 0; i < r; i++)
-	{
-		int j = 0;
-		for (j = 0; j < c; j++)
-		{
-			printf("%d ", arr[i][j]);
-		}
-		printf("\n");
-	}
-}
-void print2(int(*p)[5], int r, int c)
-{
-	int i = 0;
-	for (i = 0; i < r; i++)
-	{
-		int j = 0;
-		for (j = 0; j < c; j++)
-		{
-			printf("%d ", *(*(p + i) + j));
-		}
-		printf("\n");
-	}
-}
-int main()
-{
-	int arr[3][5] = { 1,2,3,4,5,2,3,4,5,6,3,4,5,6,7 };
-	print2(arr, 3, 5);
-	return 0;
-}
+//void print1(int arr[3][5], int r, int c)
+//{
+//	int i = 0;
+//	for (i = 0; i < r; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < c; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//}
+//void print2(int(*p)[5], int r, int c)
+//{
+//	int i = 0;
+//	for (i = 0; i < r; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < c; j++)
+//		{
+//			printf("%d ", *(*(p + i) + j));
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int arr[3][5] = { 1,2,3,4,5,2,3,4,5,6,3,4,5,6,7 };
+//	print2(arr, 3, 5);
+//	return 0;
+//}
+//一维数组传参
+//void test(int arr[])//ok
+//void test(int arr[10])//ok
+//void test(int* arr)//ok
+//void test2(int* arr[20])//ok
+//void test2(int** arr)//ok
+//int main()
+//{
+//	int arr[10] = 0;
+//	int* arr2[10] = 0;
+//	test(arr);
+//	test2(arr2);
+//}
+//二维数组传参
+//void test(int arr[3][5])//ok
+//void test(int arr[][])//no
+//void test(int arr[][5])//ok
+//void test(int* arr)//no
+//void test(int* arr[5])//no
+//void test(int (*arr)[5])//ok
+//void test(int** arr)//no
+//int main()
+//{
+//	int arr[3][5] = { 0 };
+//	test(arr);
+//}
+//函数指针——指向函数的指针
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//void calc(int(*pf)(int, int))
+//{
+//	int a = 3;
+//	int b = 5;
+//	int ret = pf(a, b);
+//	printf("%d\n", ret);
+//}
+//int main()
+//{
+//	calc(Add);
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[5] = { 0 };
+//	int(*p)[5] = &arr;
+//	//对于函数来说，&函数名和函数名都是函数的地址
+//	//int (*pf)(int, int) = &Add;//函数指针
+//	int (*pf)(int, int) = Add;//函数指针
+//	//int ret = (*pf)(2, 3);
+//	int ret = (pf)(2, 3);
+//	printf("%d\n", ret);
+//}
